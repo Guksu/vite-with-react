496e6a269eda112fc6f4f8aed54ce8f4c4621219
Guksu <yung3152@daum.net>
2023-05-08 22:27:57 +0900

    fix : createRoot 코드수정

--- src/pages/CreateRootTest.tsx
+++ src/pages/CreateRootTest.tsx
@@ -2,11 +2,15 @@ import { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 export default function CreateRootTest() {
-  const htmlSample = `<div>div div div<div id="sample">This is HTML sample</div></div>`;
+  const htmlSample = `<div>div div div<div id="sample">This is HTML sample</div><div id="sample">This is HTML sample</div></div>`;
 
   useEffect(() => {
-    const targetHTML = createRoot(document.getElementById("sample") as HTMLElement);
-    targetHTML.render(<div style={{ color: "red" }}>DOM CHANGE</div>);
+    const targetNode = document.querySelectorAll("#sample");
+
+    targetNode.forEach((node, index) => {
+      const nodeChange = createRoot(node);
+      nodeChange.render(<div style={{ color: "red" }}>index : {index} NODE CHANGE</div>);
+    });
   }, []);
 
   return <div dangerouslySetInnerHTML={{ __html: htmlSample }}></div>;
